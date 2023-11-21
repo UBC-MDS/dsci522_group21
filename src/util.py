491a207db8ff81c40a05b4b2940d30579b21898e
Jerry Yu <jyu565@gmail.com>
2023-11-21 12:56:16 -0800

    fixing function to pass unit test

--- src/util.py
+++ src/util.py
@@ -24,22 +24,22 @@ def eda_plots(data: pd.DataFrame, numerical_cols: list = [], categorical_cols: l
     numerical_plot = None
     categorical_plot = None
 
-    if len(numerical_cols) == 0:
-        logging.info("No numerical columns were provided, and no EDA plot for numerical columns will be generated.")
+    if len(categorical_cols) == 0:
+        logging.info("No categorical columns were provided, and no EDA plot for numerical columns will be generated.")
     else:
         logging.info("Generating EDA plot for numerical columns.")
-        numerical_plot = alt.Chart(data).mark_bar().encode(
+        categorical_plot = alt.Chart(data).mark_bar().encode(
             x="count()",
             y=alt.Y(alt.repeat()).type("nominal")
         ).repeat(
             categorical_cols, columns=3
         )
 
-    if len(categorical_cols) == 0:
-        logging.info("No categorical columns were provided, and no EDA plot for categorical columns will be generated.")
+    if len(numerical_cols) == 0:
+        logging.info("No numerical columns were provided, and no EDA plot for categorical columns will be generated.")
     else:
         logging.info("Generating EDA plot for categorical columns.")
-        categorical_plot = alt.Chart(data).mark_bar().encode(
+        numerical_plot = alt.Chart(data).mark_bar().encode(
             x=alt.X(alt.repeat()).type("quantitative").bin(maxbins=40),
             y="count()"
         ).repeat(
