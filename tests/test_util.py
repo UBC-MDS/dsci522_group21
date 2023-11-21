bb1f5e7d6d1421ff354e973dc0d8be06e9744f42
Jerry Yu <jyu565@gmail.com>
2023-11-21 10:54:39 -0800

    added chart type tests

--- tests/test_util.py
+++ tests/test_util.py
@@ -21,8 +21,29 @@ def test_eda_none():
     num_chart_3, cate_chart_3 = eda_plots(test_data)
 
     assert num_chart_1 is None, "Numerical chart should be None without input for numerical columns."
-    assert isinstance(cate_chart_1, alt.RepeatChart), "Incorrect data type for categorical chart."
-    assert isinstance(num_chart_2, alt.RepeatChart), "Incorrect data type for numerical chart."
+    assert cate_chart_1 is not None, "Categorical chart should not be None."
+    assert num_chart_2 is not None, "Numerical chart should not be None."
     assert cate_chart_2 is None, "Categorical chart should be None without input for numerical columns."
     assert num_chart_3 is None, "Numerical chart should be None without input for numerical columns."
     assert cate_chart_3 is None, "Categorical chart should be None without input for numerical columns."
+
+def test_eda_chart_type():
+    np.random.seed(123)
+    test_data = pd.DataFrame({
+        "cate1": ["a", "a", "b", "b", "b", "c", "a", "b", "a", "c"],
+        "cate2": ["c", "c", "b", "b", "b", "a", "c", "b", "c", "a"],
+        "num1": np.random.normal(size=10),
+        "num2": np.random.normal(size=10)
+    })
+
+    num_chart_1, cate_chart_1 = eda_plots(test_data, categorical_cols=["cate1"])
+    num_chart_2, cate_chart_2 = eda_plots(test_data, numerical_cols=["num1"])
+    num_chart_3, cate_chart_3 = eda_plots(test_data, numerical_cols=["num1"], categorical_cols=["cate1"])
+    num_chart_4, cate_chart_4 = eda_plots(test_data, numerical_cols=["num1", "num2"], categorical_cols=["cate1", "cate2"])
+
+    assert isinstance(cate_chart_1, alt.RepeatChart), "Incorrect data type for categorical chart."
+    assert isinstance(num_chart_2, alt.RepeatChart), "Incorrect data type for numerical chart."
+    assert isinstance(num_chart_3, alt.RepeatChart), "Incorrect data type for numerical chart."
+    assert isinstance(cate_chart_3, alt.RepeatChart), "Incorrect data type for categorical chart."
+    assert isinstance(num_chart_4, alt.RepeatChart), "Incorrect data type for numerical chart."
+    assert isinstance(cate_chart_4, alt.RepeatChart), "Incorrect data type for categorical chart."
