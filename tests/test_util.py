df6d6df645416b832a6ecb3013e8a1d2ac0091a7
ZEILY GARCIA <zeilygarcia2017@gmail.com>
2023-11-22 13:59:49 -0800

    minor change

--- tests/test_util.py
+++ tests/test_util.py
@@ -21,12 +21,12 @@ categorical_df = pd.DataFrame({
 
 empty_df = pd.DataFrame()
 
-# Test for correct return type
+# Correlation Heatmap test for correct return type
 def test_output_type():
     heatmap = correlation_heatmap(simple_numeric_df)
     assert isinstance(heatmap, alt.LayerChart), "Output should be an alt.LayerChart object."
 
-# Test for raising of error if method is invalid
+# Correlation Heatmap test for raising of error if method is invalid
 def test_invalid_method():
     try:
         heatmap = correlation_heatmap(simple_numeric_df, method='invalid')
@@ -34,30 +34,30 @@ def test_invalid_method():
     except ValueError:
         pass
 
-# Test for empty dataframe input
+# Correlation Heatmap test for empty dataframe input
 def test_empty_dataframe():
     heatmap = correlation_heatmap(empty_df, method='spearman')
     assert heatmap.data.empty, "Function should return an empty heatmap for an empty DataFrame."
 
-# Test for categorical columns input 
+# Correlation Heatmap test for categorical columns input 
 def test_no_numerical_columns():
     heatmap = correlation_heatmap(categorical_df)
     assert heatmap.data.empty, "Function should return an empty heatmap for non numerical DataFrame."
 
-# Test for correct type for calculated values
+# Correlation Heatmap test for correct type for calculated values
 def test_data_type_in_heatmap():
     heatmap = correlation_heatmap(simple_numeric_df)
     for val in heatmap.data['correlation']:
         assert isinstance(val, (int, float)), "correlation values should be int or float data types."
 
-# Test for correct calculation
+# Correlation Heatmap test for correct calculation
 def test_correct_correlation_calculation():
     heatmap = correlation_heatmap(simple_numeric_df)
     expected_corr = simple_numeric_df.corr(method='pearson').stack().reset_index(name='correlation')
     actual_corr = heatmap.data
     assert expected_corr['correlation'].equals(actual_corr['correlation']), "correlation values are not being calculated correctly."
 
-# Test for correct labels
+# Correlation Heatmap test for correct labels
 def test_heatmap_labels():
     heatmap = correlation_heatmap(simple_numeric_df)
     x_labels = set(heatmap.data['num_variable_0'].unique())
