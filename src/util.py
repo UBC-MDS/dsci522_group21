1b99afcc2a284f80bad9066d7ddc4a1be3d8484c
John Shiu <asbjchk.academic@gmail.com>
2023-11-21 23:42:42 -0800

    added error handling

--- src/util.py
+++ src/util.py
@@ -1,6 +1,8 @@
 import pandas as pd
+from sklearn.compose import ColumnTransformer
+from sklearn.linear_model import LogisticRegression
 
-def plot_logistic_regression_feature_importance(fitted_lr_pipe, precision=3, cmap="PiYG", vmin=None, vmax=None, caption=None):
+def plot_logistic_regression_feature_importance(fitted_lr_pipe, head=None, precision=3, cmap="PiYG", vmin=None, vmax=None, caption=None):
     """
     Returns two EDA plots, one for numerical columns and another for categorical columns.
 
@@ -19,21 +21,38 @@ def plot_logistic_regression_feature_importance(fitted_lr_pipe, precision=3, cma
         two EDA plots, one for numerical columns and another for categorical columns
     """
 
+    if len(fitted_lr_pipe.named_steps) != 2:
+        raise TypeError("`fitted_lr_pipe` is expected to have exactly two components: ColumnTransformer and LogisticRegression")
+    
     ct, lr = fitted_lr_pipe.named_steps.values()
+    if not isinstance(ct, ColumnTransformer):
+        raise TypeError("1st component in the `fitted_lr_pipe` is expected to be a ColumnTransformer")
+    if not isinstance(ct, LogisticRegression):
+        raise TypeError("2nd component in the `fitted_lr_pipe` is expected to be a LogisticRegression")
+    if len(ct.named_transformers_) == 0:
+        raise TypeError("ColumnTransformer has no Encoder")
+    try:
+        coef = lr.coef_[0]
+    except AttributeError as e:
+        raise TypeError("LogisticRegression is not fitted (`fitted_lr_pipe.fit(X_train, y_train))")
+        
     features = []
     for enc in ct.named_transformers_.values():
         features += enc.get_feature_names_out().tolist()
-
+    if len(features) != len(coef):
+        raise ValueError("The number of features does not match the number of coefficients")
+    
     feature_importance = pd.DataFrame({
         'feature': features,
-        'coef': lr.coef_[0].tolist(),
-        'coef_abs': map(abs, lr.coef_[0].tolist())
+        'coef': coef.tolist(),
+        'coef_abs': abs(coef).tolist())
     })
 
     feature_importance = (
         feature_importance
         .sort_values('coef_abs', ascending=False)
         .drop(columns=["coef_abs"])
+        .head(head)
         .style.format(
             precision=3)
         .background_gradient(
