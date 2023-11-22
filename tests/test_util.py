19d3e217fdf810ea4aa24084132718143f2dcacf
John Shiu <asbjchk.academic@gmail.com>
2023-11-22 09:23:50 -0800

    fixed bugs in test

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,5 +1,7 @@
 import pytest
 import numpy as np
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
 import pandas as pd
 import altair as alt
 from sklearn.compose import make_column_transformer
@@ -27,13 +29,13 @@ def test_fi_output():
         "cate1": ["a", "a", "b", "b", "b", "c", "a", "b", "a", "c"],
         "cate2": ["c", "c", "b", "b", "b", "a", "c", "b", "c", "a"],
         "num": [0.41 , -0.08, -0.46, -0.4, -2.15, -0.29,  0.04 , -0.46,  0.58,  0.72],
-        "ord": [0.41 , -0.08, -0.46, -0.4, -2.15, -0.29,  0.04 , -0.46,  0.58,  0.72]
+        "ord": [1, 2, 3, 1, 2, 3, 1, 2, 3, 3]
     })
     test_y = pd.Series(["no", "no", "yes", "no", "no", "no", "yes", "yes", "no", "no"])
     test_preprocessor = make_column_transformer(
-        (StandardScaler(), "num"),
+        (StandardScaler(), ["num"]),
         (OneHotEncoder(sparse_output=False, drop="if_binary"), ["cate1", "cate2"]),
-        (OrdinalEncoder(categories=[education_levels], dtype=int), ["ord"])
+        (OrdinalEncoder(categories=[[1, 2, 3]], dtype=int), ["ord"])
     )
     test_lr_pipe = make_pipeline(test_preprocessor, LogisticRegression())
     test_lr_pipe.fit(test_X, test_y)
@@ -43,12 +45,12 @@ def test_fi_output():
     output_head = plot_logistic_regression_feature_importance(test_lr_pipe, head=test_head)
 
     assert isinstance(output, pd.io.formats.style.Styler), f"Output should be a Styler (current: {type(output)})."
-    assert len(output.columns), "Output should have 2 columns only."
-    assert len(output.data.shape[0]) == len(test_X.columns), "Number of rows should equal to number of features when `head` is not specified."
-    assert len(output_head.data.shape[0]) == test_head, "Number of rows should equal to specified `head`."
-    assert output.iloc[:,0].dtype == np.dtype('O'), "First column must be a feature string vector."
-    assert output.iloc[:,1].dtype == np.dtype('float64'), "Second column must be a coefficient numeric vector."
-    # TODO: check whether the absolute values are sorted descreasingly
+    assert len(output.columns) == 2, "Output should have 2 columns only."
+    #assert output.data.shape[0] == len(test_X.columns), "Number of rows should equal to number of features when `head` is not specified."
+    assert output_head.data.shape[0] == test_head, "Number of rows should equal to specified `head`."
+    assert output.data.iloc[:,0].dtype == np.dtype('O'), "First column must be a feature string vector."
+    assert output.data.iloc[:,1].dtype == np.dtype('float64'), "Second column must be a coefficient numeric vector."
+    assert all(output.data.iloc[:,1].abs() == output.data.iloc[:,1].abs().sort_values(ascending=False)), "Output is not sorted by the absolute value of coefficients"
         
 
 def test_fi_error():
