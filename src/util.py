b123021b8865e0a1c2bfc3058cf3483cf66afaf6
John Shiu <asbjchk.academic@gmail.com>
2023-11-24 02:18:45 -0800

    updated the docstring

--- src/util.py
+++ src/util.py
@@ -8,55 +8,32 @@ import os
 def load_data_and_split(file_path, test_size=0.2, random_state=42, delimiter=None):
     
     """
-    Load data from a specified file and split it into training and testing datasets.
-
-    This function is designed to work with a variety of file formats including CSV, TXT, and JSON.
-    It reads the data into a pandas DataFrame and splits it into two separate DataFrames for training and testing purposes.
-    
-    The function assumes that the input file is structured with features and an optional target variable. If a delimiter
-    is not specified for CSV/TXT files, the function defaults to using a comma (','). For JSON files, the delimiter
-    parameter is ignored.
+    Load data from a specified CSV/TXT/JSON file and split it into training and testing datasets.
 
     Parameters:
     ----------
     file_path : str
         The path to the data file.
     
-    test_size : float, optional (default=0.2)
+    test_size : float, default=0.2
         The proportion of the dataset to include in the test split. Should be between 0.0 and 1.0.
     
-    random_state : int, optional (default=42)
+    random_state : int, default=42
         Controls the shuffling applied to the data before applying the split. Pass an int for reproducible output across multiple function calls.
     
-    delimiter : str, optional (default=None)
+    delimiter : str, default=None
         The delimiter used in the file if it's a CSV or TXT. If None, defaults to ',' for CSV.
 
     Returns:
     -------
-    train_df : DataFrame
-        The training dataset, containing a subset of rows from the original data file.
+    (pd.DataFrame, pd.DataFrame)
+        A tuple of the training and testing dataset.
     
-    test_df : DataFrame
-        The testing dataset, containing the remaining rows not included in the training dataset.
-
     Raises:
     ------
     ValueError
         If the file type is not supported (not in CSV, TXT, or JSON formats).
 
-    Examples:
-    --------
-    To split a CSV file with a comma as a delimiter:
-
-    >>> train_df, test_df = load_data_and_split('data/bank-full.csv', test_size=0.25, random_state=123)
-
-    To split a CSV file with a semi-colon as a delimiter:
-
-    >>> train_df, test_df = load_data_and_split('data/bank-full.csv', test_size=0.25, random_state=123, delimiter=';')
-
-    Note:
-    -----
-    The file path provided to the function must be accessible from the current working directory.
     """
     
     # Determine the file type
