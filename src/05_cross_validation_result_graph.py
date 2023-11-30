e9e10ea7d1bab45dada96eea14697be44a2812dd
John Shiu <asbjchk.academic@gmail.com>
2023-11-30 14:34:28 -0800

    refactored the 05 function

--- src/05_cross_validation_result_graph.py
+++ src/05_cross_validation_result_graph.py
@@ -1,25 +1,21 @@
 import click
 import pandas as pd
 
-def process_data(file_path):
-    cross_val_results = pd.read_csv(file_path, header=[0, 1], index_col=0)
+@click.command()
+@click.option('--cv-results', help='Path to the cross-validation results CSV file.')
+@click.option('--output-cv-means', help='Path to save the mean cross-validation scores as a CSV file.')
 
-    Fig_6 = cross_val_results.xs(
-        'mean', 
-        axis='columns', 
+def main(cv_results, output_cv_means):
+    """ Process cross-validation results and save the mean values to a CSV file. """
+
+    res = pd.read_csv(cv_results, header=[0, 1], index_col=0)
+
+    res.xs(
+        'mean',
+        axis='columns',
         level=1
-    )
-    return Fig_6
+    ).to_csv(output_cv_means)
 
-@click.command(context_settings={'ignore_unknown_options': True})
-@click.option('--file-path', default='../data/processed/cv_results.csv', help='Path to the cv_results.csv file.')
-@click.option('--output-path', default='../data/processed/Fig_6.csv', help='Output path for the Excel file.')
-@click.argument('args', nargs=-1, type=click.UNPROCESSED)
-
-def main(file_path, output_path, args):
-    Fig_6 = process_data(file_path)
-
-    Fig_6.to_csv(output_path)
 
 if __name__ == "__main__":
     main()
